d40ee7d118f30b15564001c18ed5995b06d98c9e
westerndundrey <hijackedpuffin@gmail.com>
2023-08-20 14:38:34 +0800

    Rust Guessing Game

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("The secret number is: {}", secret_number);
 loop {
     println!("Please input a number.");
 
@@ -16,8 +15,11 @@ loop {
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().
-    expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+    
 
     println!("You guessed: {}", guess);
 
